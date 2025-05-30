9895d3053bfcb5b783ddffe1fa871446f202d15f
Ismael Santos <99123@globalia.com>
2025-05-30 21:34:36 +0200

    Initial commit ArtMetrics

---
+++ public/41155975-8c3421966b792d7ffa10.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[396],{6204:function(t,a,e){e.d(a,{LCd:function(){return u}});var r=e(3621);function u(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=41155975-8c3421966b792d7ffa10.js.map
